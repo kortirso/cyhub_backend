299b344ca278cbe66945ea75bf3763a118d720cf
kortirso <kortirso@gmail.com>
2017-11-06 12:55:44 +0200

    modify login through api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,8 +7,19 @@ module Api
             private
 
             def authenticate
-                @user = User.active.find_by(email: params[:email])
-                render json: { error: 'Unauthorized' }, status: 401 if @user.nil? || !@user.valid_password?(params[:password])
+                @user = User.find_by(email: params[:email])
+                user_creation if @user.nil?
+                check_user_activity
+                render json: { error: 'Unauthorized' }, status: 401 unless @user.valid_password?(params[:password])
+            end
+
+            def user_creation
+                @user = User.new email: params[:email], password: params[:password], active: true
+                render json: { error: 'User creation error' }, status: 401 unless @user.save
+            end
+
+            def check_user_activity
+                render json: { error: 'User is not actives' }, status: 401 unless @user.active?
             end
         end
     end
