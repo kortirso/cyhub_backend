a363dea8aa46e5c190873802724f9855af941461
kortirso <kortirso@gmail.com>
2018-04-11 09:22:59 +0700

    add admin page

---
+++ frontend/components/navigation_admin/navigation_admin.js
@@ -0,0 +1,1 @@
+import "./navigation_admin.css";
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,9 +3,11 @@ import "images";
 
 import "components/page/page";
 import "components/navigation/navigation";
+import "components/navigation_admin/navigation_admin";
 import "components/footer/footer";
 
 import "components/welcome/welcome";
+import "components/admin/admin";
 
 import Rails from "rails-ujs";
 
