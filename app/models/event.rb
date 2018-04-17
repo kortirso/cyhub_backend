7e0bc9e519c906932dc7d5dcfdf080860b1084bd
kortirso <kortirso@gmail.com>
2018-04-17 16:11:37 +0700

    change image rendering of events for api

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   has_one_attached :image
 
   validates :name, :description, :link, presence: true
+
+  def image_content
+    image.attached? ? Base64.encode64(image.attachment.blob.download) : nil
+  end
 end
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-
-  attributes :id, :name, :description, :link, :started_at, :finished_at, :link_to_file
+  attributes :id, :name, :description, :link, :started_at, :finished_at, :image_content
 
   def started_at
     object.started_at.try(:strftime, '%Y/%m/%d %H:%M')
@@ -9,10 +7,5 @@ class EventSerializer < ActiveModel::Serializer
 
   def finished_at
     object.finished_at.try(:strftime, '%Y/%m/%d %H:%M')
-  end
-
-  def link_to_file
-    return nil unless object.image.attached?
-    rails_blob_url(object.image, disposition: 'attachment', only_path: true)
   end
 end
