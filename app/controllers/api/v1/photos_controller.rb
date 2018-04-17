1c2a6463a1d94adc9453d6d231be95c14be5a569
kortirso <kortirso@gmail.com>
2018-04-17 23:31:45 +0700

    add api endpoint for saving new photos

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,10 +1,26 @@
 module Api
   module V1
     class PhotosController < Api::V1::BaseController
-      skip_before_action :authenticate
+      skip_before_action :verify_authenticity_token, only: %i[create]
+      skip_before_action :authenticate, only: %i[index]
+      before_action :check_member, only: %i[create]
 
       def index
         render json: { photos: ActiveModel::Serializer::CollectionSerializer.new(Photo.order(id: :asc).with_attached_image.limit(15), each_serializer: PhotoSerializer) }, status: 200
+      end
+
+      def create
+        photo = Photo.create(photo_params.merge(member: @user.member))
+        photo.image.attach(io: File.open(params['photo']['image']), filename: "image.#{params['photo']['image'].split('.')[-1]}")
+        render json: { photos: ActiveModel::Serializer::CollectionSerializer.new(Photo.order(id: :asc).with_attached_image.limit(15), each_serializer: PhotoSerializer) }, status: 200
+      end
+
+      private def check_member
+        render json: { error: 'User does not have membership' }, status: 404 unless @user.member?
+      end
+
+      private def photo_params
+        params.require(:photo).permit(:description)
       end
     end
   end
